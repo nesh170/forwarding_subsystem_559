75e5a77b63a4ce94108590200d7e2e1a1a7704c2
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-12-12 15:58:51 -0500

    pushing the mif file

--- forwarding_test_subsystem.vhd
+++ forwarding_test_subsystem.vhd
@@ -15,13 +15,17 @@ ENTITY forwarding_test_subsystem IS
 			cntl_block_out          : OUT STD_LOGIC_VECTOR(23 DOWNTO 0);
 			cb_write                : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
 			high_priority_out       : OUT STD_LOGIC;
-			end_test						: OUT STD_LOGIC
+			end_test						: OUT STD_LOGIC;
+			
+			
+			--debugging ports
+			memory_output : OUT STD_LOGIC_VECTOR(23 DOWNTO 0)
 			);
 END forwarding_test_subsystem;
 
 ARCHITECTURE tester OF forwarding_test_subsystem IS 
 	TYPE state_type is 
-		(wait_state,process_stage,write_frame_stage,increment_cb_stage,check_cb_stage,end_stage);
+		(wait_state,process_stage,write_frame_stage,increment_cb_stage,check_cb_stage,end_state);
 	SIGNAL state_reg,next_state : state_type;
 	SIGNAL register_input_cb_counter_1,register_output_cb_counter_1,register_input_cb_counter_2,register_output_cb_counter_2 : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL ctrl_write_counter_register : STD_LOGIC;
@@ -102,6 +106,8 @@ BEGIN
 		q => cb_memory_output_1
 	);
 	
+	memory_output <= cb_memory_output_1;
+	
 	cb_memory_2_inst: cb_memory_2 PORT MAP (
 		aclr => reset,
 		address => register_output_cb_counter_2(5 DOWNTO 0),
@@ -157,11 +163,13 @@ BEGIN
 		end if;
 	END PROCESS;
 	
-	PROCESS(state_reg,cb_memory_output_1,cb_memory_output_2,register_output_cb_counter_1,register_output_cb_counter_2, current_memory_address_1, current_memory_address_2, frame_endpoint_1, frame_endpoint_2, register_output_frame_counter_1, register_output_frame_counter_2, register_output_frame_endpoint_1, register_output_frame_endpoint_2)
+	PROCESS(state_reg,start_test,cb_memory_output_1,cb_memory_output_2,register_output_cb_counter_1,register_output_cb_counter_2, current_memory_address_1, current_memory_address_2, frame_endpoint_1, frame_endpoint_2, register_output_frame_counter_1, register_output_frame_counter_2, register_output_frame_endpoint_1, register_output_frame_endpoint_2)
 	BEGIN
 		CASE state_reg IS
 			WHEN wait_state =>
 				register_input_frame_counter_1 <= register_output_frame_counter_1;
+				register_input_cb_counter_1 <= register_output_cb_counter_1;
+				register_input_cb_counter_2 <= register_output_cb_counter_2;
 				register_input_frame_counter_2 <= register_output_frame_counter_2;
 				register_input_frame_endpoint_1 <= register_output_frame_endpoint_1;
 				register_input_frame_endpoint_2 <= register_output_frame_endpoint_2;
@@ -171,32 +179,49 @@ BEGIN
 				ctrl_write_frame_endpoint_2_register <= '0';
 				frame_write_1_fwd <= '0';
 				frame_write_2_fwd <= '0';
+				cb_write_1_fwd <= '0';
+				cb_write_2_fwd <= '0';
+				ctrl_write_counter_register <= '0';
 				if(start_test = '1' and (cb_memory_output_1 /= x"000000" or cb_memory_output_2 /= x"000000")) then
 					next_state <= process_stage;
 				else
 					next_state <= wait_state;
 				end if;
 			when process_stage =>
+				frame_write_1_fwd <= '0';
+				frame_write_2_fwd <= '0';
 				register_input_frame_counter_1 <= register_output_frame_counter_1;
 				register_input_frame_counter_2 <= register_output_frame_counter_2;
+				register_input_cb_counter_1 <= register_output_cb_counter_1;
+				register_input_cb_counter_2 <= register_output_cb_counter_2;
 				ctrl_write_frame_1_register <= '0';
-				ctrl_write_frame_2_register <= '0';	
+				ctrl_write_frame_2_register <= '0';
+				ctrl_write_counter_register <= '0';
+				register_input_frame_endpoint_1 <= std_logic_vector(to_unsigned(to_integer(unsigned(register_output_frame_endpoint_1)) + to_integer(unsigned(cb_memory_output_1(11 DOWNTO 0))),register_input_frame_endpoint_1'length));	
+				register_input_frame_endpoint_2 <= std_logic_vector(to_unsigned(to_integer(unsigned(register_output_frame_endpoint_2)) + to_integer(unsigned(cb_memory_output_2(11 DOWNTO 0))),register_input_frame_endpoint_2'length));
 				if(cb_memory_output_1 /= x"0000") then
 					cb_write_1_fwd <= '1';
 					ctrl_write_frame_endpoint_1_register <= '1';
-					register_input_frame_endpoint_1 <= std_logic_vector(to_unsigned(to_integer(unsigned(register_output_frame_endpoint_1)) + to_integer(unsigned(cb_memory_output_1(11 DOWNTO 0))),register_input_frame_endpoint_1'length));
-					register_input_frame_endpoint_2 <= register_output_frame_endpoint_2;
+				else
+					cb_write_1_fwd <= '0';
+					ctrl_write_frame_endpoint_1_register <= '0';
 				end if;
 				if(cb_memory_output_2 /= x"0000") then
 					cb_write_2_fwd <= '1';
 					ctrl_write_frame_endpoint_2_register <= '1';
-					register_input_frame_endpoint_1 <= register_output_frame_endpoint_1;
-					register_input_frame_endpoint_2 <= std_logic_vector(to_unsigned(to_integer(unsigned(register_output_frame_endpoint_2)) + to_integer(unsigned(cb_memory_output_2(11 DOWNTO 0))),register_input_frame_endpoint_2'length));
+				else
+					cb_write_2_fwd <= '0';
+					ctrl_write_frame_endpoint_2_register <= '0';
 				end if;
 				next_state <= write_frame_stage;
 			when write_frame_stage =>
+				cb_write_1_fwd <= '0';
+				cb_write_2_fwd <= '0';
+				register_input_cb_counter_1 <= register_output_cb_counter_1;
+				register_input_cb_counter_2 <= register_output_cb_counter_2;
 				ctrl_write_frame_endpoint_1_register <= '0';
 				ctrl_write_frame_endpoint_2_register <= '0';
+				ctrl_write_counter_register <= '0';
 				register_input_frame_endpoint_1 <= register_output_frame_endpoint_1;
 				register_input_frame_endpoint_2 <= register_output_frame_endpoint_2;
 				if(current_memory_address_1 = frame_endpoint_1 and current_memory_address_2 = frame_endpoint_2) then
@@ -234,6 +259,8 @@ BEGIN
 					next_state <= write_frame_stage;
 				end if;
 			when increment_cb_stage =>
+				cb_write_1_fwd <= '0';
+				cb_write_2_fwd <= '0';
 				ctrl_write_frame_endpoint_1_register <= '0';
 				ctrl_write_frame_endpoint_2_register <= '0';
 				ctrl_write_frame_1_register <= '0';
@@ -249,10 +276,15 @@ BEGIN
 				register_input_cb_counter_2 <= std_logic_vector(to_unsigned(to_integer(unsigned(register_output_cb_counter_2)) + 1,register_input_cb_counter_2'length));
 				next_state <= check_cb_stage;
 			when check_cb_stage => 
+				cb_write_1_fwd <= '0';
+				cb_write_2_fwd <= '0';
+				ctrl_write_counter_register <= '0';
 				register_input_frame_counter_1 <= register_output_frame_counter_1;
 				register_input_frame_counter_2 <= register_output_frame_counter_2;
 				register_input_frame_endpoint_1 <= register_output_frame_endpoint_1;
 				register_input_frame_endpoint_2 <= register_output_frame_endpoint_2;
+				register_input_cb_counter_1 <= register_output_cb_counter_1;
+				register_input_cb_counter_2 <= register_output_cb_counter_2;
 				ctrl_write_frame_endpoint_1_register <= '0';
 				ctrl_write_frame_endpoint_2_register <= '0';
 				ctrl_write_counter_register <= '0';
@@ -263,21 +295,36 @@ BEGIN
 				if((cb_memory_output_1 /= x"0000" or cb_memory_output_2 /= x"0000")) then 
 					next_state <= process_stage;
 				else
-					next_state <= end_stage;
+					next_state <= end_state;
 				end if;	
-			when end_stage =>
+			when end_state =>
 				--do nothing :/ just turn on the lights
+				cb_write_1_fwd <= '0';
+				cb_write_2_fwd <= '0';
 				ctrl_write_frame_1_register <= '0';
 				ctrl_write_frame_2_register <= '0';	
 				ctrl_write_frame_endpoint_1_register <= '0';
 				ctrl_write_frame_endpoint_2_register <= '0';
 				frame_write_1_fwd <= '0';
 				frame_write_2_fwd <= '0';
+				ctrl_write_counter_register <= '0';
 				register_input_frame_counter_1 <= register_output_frame_counter_1;
 				register_input_frame_counter_2 <= register_output_frame_counter_2;
 				register_input_frame_endpoint_1 <= register_output_frame_endpoint_1;
 				register_input_frame_endpoint_2 <= register_output_frame_endpoint_2;
-			
+				register_input_cb_counter_1 <= register_output_cb_counter_1;
+				register_input_cb_counter_2 <= register_output_cb_counter_2;
+				next_state <= end_state;
+		END CASE;
+	END PROCESS;
+	
+	PROCESS(state_reg)
+	BEGIN
+		CASE state_reg IS
+			WHEN end_state =>
+				end_test <= '1';
+			WHEN others =>
+				end_test <= '0';
 		END CASE;
 	END PROCESS;
 	
